dae59ac7fafadcb7f622c02e71584e39576b2ae2
Alexander Widak <deltatree.widak@googlemail.com>
2024-06-21 19:53:30 +0200

    Update redsocks-create-conf.sh

--- dockerbuild/redsocks-create-conf.sh
+++ dockerbuild/redsocks-create-conf.sh
@@ -1,7 +1,8 @@
-echo "Creating redsocks configuration file using proxy ${RS_PROXY_HOST}:${RS_PROXY_PORT}, listen ${RS_LISTEN_HOST}:${RS_LISTEN_PORT}..."
+echo "Creating redsocks configuration file using proxy ${RS_PROXY_HOST}:${RS_PROXY_PORT}, listening ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTP} and ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTPS}..."
 sed -e "s|\${RS_PROXY_HOST}|${RS_PROXY_HOST}|" \
     -e "s|\${RS_PROXY_PORT}|${RS_PROXY_PORT}|" \
-    -e "s|\${RS_LISTEN_PORT}|${RS_LISTEN_PORT}|" \
+    -e "s|\${RS_LISTEN_PORT_HTTP}|${RS_LISTEN_PORT_HTTP}|" \
+    -e "s|\${RS_LISTEN_PORT_HTTPS}|${RS_LISTEN_PORT_HTTPS}|" \
     -e "s|\${RS_LISTEN_HOST}|${RS_LISTEN_HOST}|" \
     -e "s|\${RS_DAEMON}|${RS_DAEMON}|" \
     -e "s|\${RS_LOG_DEBUG}|${RS_LOG_DEBUG}|" \
