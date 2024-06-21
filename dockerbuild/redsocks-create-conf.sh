c967f9a56da5da9730cfa08047279dbaf0626673
Alexander Widak <deltatree.widak@googlemail.com>
2024-06-21 19:47:46 +0200

    Update redsocks-create-conf.sh

--- dockerbuild/redsocks-create-conf.sh
+++ dockerbuild/redsocks-create-conf.sh
@@ -4,6 +4,8 @@ sed -e "s|\${RS_PROXY_HOST}|${RS_PROXY_HOST}|" \
     -e "s|\${RS_LISTEN_PORT}|${RS_LISTEN_PORT}|" \
     -e "s|\${RS_LISTEN_HOST}|${RS_LISTEN_HOST}|" \
     -e "s|\${RS_DAEMON}|${RS_DAEMON}|" \
+    -e "s|\${RS_LOG_DEBUG}|${RS_LOG_DEBUG}|" \
+    -e "s|\${RS_LOG_INFO}|${RS_LOG_INFO}|" \
     ${RS_TEMPLATE} > ${RS_CONF}
 echo "Generated configuration:"
 cat ${RS_CONF}
