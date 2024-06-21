6b47f1fd39c996cf9600a3f0a440a8cda5cb3481
Alexander Widak <deltatree.widak@googlemail.com>
2024-06-21 22:25:44 +0200

    Update redsocks-fw.sh

--- dockerbuild/redsocks-fw.sh
+++ dockerbuild/redsocks-fw.sh
@@ -1,47 +1,28 @@
 #!/bin/sh
 
 fw_setup() {
-  iptables-legacy -t nat -N REDSOCKS
+  iptables -t nat -N REDSOCKS
 
   for item in $(echo ${RS_WHITELIST} | sed "s/,/ /g")
   do
-      iptables-legacy -t nat -A REDSOCKS -d $item -j RETURN
+      iptables -t nat -A REDSOCKS -d $item -j RETURN
   done
-
-  iptables-legacy -t nat -A REDSOCKS -p tcp --dport ${RS_LISTEN_PORT_HTTP} -s ${RS_LISTEN_HOST} -j RETURN
-  iptables-legacy -t nat -A REDSOCKS -p tcp --dport ${RS_LISTEN_PORT_HTTPS} -s ${RS_LISTEN_HOST} -j RETURN
 
   for item in $(echo ${RS_PORTS_HTTP} | sed "s/,/ /g")
   do
-      iptables-legacy -t nat -A REDSOCKS -p tcp --dport $item -j DNAT --to-destination ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTP}
-
-      if [ -z "${RS_TARGET_NET}" ]
-      then
-        iptables-legacy -t nat -A PREROUTING -p tcp --dport $item -j REDSOCKS
-        iptables-legacy -t nat -A OUTPUT -p tcp --dport $item -j REDSOCKS
-      else
-        iptables-legacy -t nat -A PREROUTING -i ${RS_TARGET_NET} -p tcp --dport $item -j REDSOCKS
-        iptables-legacy -t nat -A OUTPUT -i ${RS_TARGET_NET} -p tcp --dport $item -j REDSOCKS
-      fi
+      iptables -t nat -A REDSOCKS -p tcp --dport $item -j DNAT --to-destination ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTP}
   done
 
   for item in $(echo ${RS_PORTS_HTTPS} | sed "s/,/ /g")
   do
-      iptables-legacy -t nat -A REDSOCKS -p tcp --dport $item -j DNAT --to-destination ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTPS}
-
-      if [ -z "${RS_TARGET_NET}" ]
-      then
-        iptables-legacy -t nat -A PREROUTING -p tcp --dport $item -j REDSOCKS
-        iptables-legacy -t nat -A OUTPUT -p tcp --dport $item -j REDSOCKS
-      else
-        iptables-legacy -t nat -A PREROUTING -i ${RS_TARGET_NET} -p tcp --dport $item -j REDSOCKS
-        iptables-legacy -t nat -A OUTPUT -i ${RS_TARGET_NET} -p tcp --dport $item -j REDSOCKS
-      fi
+      iptables -t nat -A REDSOCKS -p tcp --dport $item -j DNAT --to-destination ${RS_LISTEN_HOST}:${RS_LISTEN_PORT_HTTPS}
   done
+
+  iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
 }
 
 fw_clear() {
-  iptables-legacy-save | grep -v REDSOCKS | iptables-legacy-restore
+  iptables-save | grep -v REDSOCKS | iptables-restore
 }
 
 case "$1" in
